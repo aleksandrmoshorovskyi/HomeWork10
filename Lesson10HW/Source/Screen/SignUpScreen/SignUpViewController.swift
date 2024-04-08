a1574c2e083e13a0e639b5ab50bf9a8da8b4d685
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-04-08 12:23:25 +0300

    return

--- Lesson10HW/Source/Screen/SignUpScreen/SignUpViewController.swift
+++ Lesson10HW/Source/Screen/SignUpScreen/SignUpViewController.swift
@@ -15,8 +15,6 @@ class SignUpViewController: UIViewController {
     var cardNumberDelegate = CardNumberTextFieldDelegate()
     var cvvDelegate = CVVTextFieldDelegate()
     
-    var commonTextFieldDelegate = CommonTextFieldDelegate()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,29 +34,8 @@ class SignUpViewController: UIViewController {
         contentView.cardNumberTextField.keyboardType = .numberPad
         contentView.cvvTextField.keyboardType = .numberPad
         
-        // MARK: - returnKeyType
-        contentView.firstNameTextField.returnKeyType = .done
-        contentView.lastNameTextField.returnKeyType = .done
-        contentView.emailNameTextField.returnKeyType = .done
-        contentView.passwordTextField.returnKeyType = .done
-        contentView.confirmPasswordTextField.returnKeyType = .done
-        contentView.countryTextField.returnKeyType = .done
-        contentView.cityTextField.returnKeyType = .done
-        contentView.addressTextField.returnKeyType = .done
-        contentView.expDateTextField.returnKeyType = .done
-        
         // MARK: - delegates
         contentView.cardNumberTextField.delegate = cardNumberDelegate
         contentView.cvvTextField.delegate = cvvDelegate
-        
-        contentView.firstNameTextField.delegate = commonTextFieldDelegate
-        contentView.lastNameTextField.delegate = commonTextFieldDelegate
-        contentView.emailNameTextField.delegate = commonTextFieldDelegate
-        contentView.passwordTextField.delegate = commonTextFieldDelegate
-        contentView.confirmPasswordTextField.delegate = commonTextFieldDelegate
-        contentView.countryTextField.delegate = commonTextFieldDelegate
-        contentView.cityTextField.delegate = commonTextFieldDelegate
-        contentView.addressTextField.delegate = commonTextFieldDelegate
-        contentView.expDateTextField.delegate = commonTextFieldDelegate
     }
 }
